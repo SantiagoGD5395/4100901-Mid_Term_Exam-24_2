4e3cfb10f7f3fea0a184bb36a2335f167e9b0971
SantiagoGD5395 <sagomezdu@unal.edu.co>
2024-12-19 10:26:10 -0500

    Correcciones examen

--- Src/main.c
+++ Src/main.c
@@ -12,10 +12,10 @@ typedef enum {
     PERM_UNLOCK
 } DoorState_t;
 
-DoorState_t current_state = LOCKED;
-uint32_t unlock_timer = 0;
+static DoorState_t current_state = LOCKED;
+static uint32_t unlock_timer = 0;
 
-void run_state_machine(void) {
+static run_state_machine(void) {
     switch (current_state) {
         case LOCKED:
             // No periodic action in locked state
@@ -24,6 +24,7 @@ void run_state_machine(void) {
             if (systick_GetTick() - unlock_timer >= TEMP_UNLOCK_DURATION) {
                 gpio_set_door_led_state(0); // Turn off door state LED
                 current_state = LOCKED;
+                usart2_send_string("Door Locked\r\n");
             }
             break;
         case PERM_UNLOCK:
@@ -32,22 +33,31 @@ void run_state_machine(void) {
     }
 }
 
-void handle_event(uint8_t event) {
-    if (event == 1) { // Single button press
-        gpio_set_door_led_state(1); // Turn on door state LED
-        current_state = TEMP_UNLOCK;
-        unlock_timer = systick_GetTick();
-    } else if (event == 2) { // Double button press
-        gpio_set_door_led_state(1); // Turn on door state LED
-        current_state = PERM_UNLOCK;
-    } else if (event == 'O') { // UART OPEN command
-        gpio_set_door_led_state(1); // Turn on door state LED
-        current_state = TEMP_UNLOCK;
-        unlock_timer = systick_GetTick();
-    } else if (event == 'C') { // UART CLOSE command
-        gpio_set_door_led_state(0); // Turn off door state LED
-        current_state = LOCKED;
+static void handle_event(uint8_t event) {
+    switch (event)
+    {
+    case 
+        if (event == 1) { // Single button press
+            gpio_set_door_led_state(1); // Turn on door state LED
+            current_state = TEMP_UNLOCK;
+            unlock_timer = systick_GetTick();
+        } else if (event == 2) { // Double button press
+            gpio_set_door_led_state(1); // Turn on door state LED
+            current_state = PERM_UNLOCK;
+        } else if (event == 'O') { // UART OPEN command
+            gpio_set_door_led_state(1); // Turn on door state LED
+            current_state = TEMP_UNLOCK;
+            unlock_timer = systick_GetTick();
+        } else if (event == 'C') { // UART CLOSE command
+            gpio_set_door_led_state(0); // Turn off door state LED
+            current_state = LOCKED;
+        }
+            break;
+    
+    default:
+        break;
     }
+    
 }
 
 int main(void) {
